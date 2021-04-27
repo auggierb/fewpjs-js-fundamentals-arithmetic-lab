faa9b74f5eea08ae7b9d171cf53e94f989cebb02
August <auggierutter@gmail.com>
2021-04-27 15:20:55 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+// Write your code h
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2 
+
+let random = Math.ceil(Math.random() + 1)
+let num3 = 100
+let num4 = 2
+let mod = Number.parseInt('100','2')
+
+let max = Math.max(5,19,7,4,20);
+
